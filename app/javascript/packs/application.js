56d8e1143bc22aecfbb78fb67a2e23631c0321a6
Hugo Daniel <hugodaniel.icam@gmail.com>
2020-08-19 10:27:31 +0200

    Lecture Geocoding

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,16 +1,13 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("turbolinks").start()
 
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -30,9 +27,11 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initMapbox();
+  initAutocomplete();
 });
